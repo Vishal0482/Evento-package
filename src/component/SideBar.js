77381d262c259336200cef0c9ca9a6ae86cd4ceb
Vishal Parmar <vishal482parmar@gmail.com>
2022-07-05 21:59:19 +0530

    sidebar component implemented

---
+++ src/component/SideBar.js
@@ -0,0 +1,155 @@
+import React from "react";
+import logoImage from "../assest/svg/logo.svg";
+import userImage from "../assest/images/user-2.png";
+
+function SideBar() {
+  return (
+    <div class="main flex min-h-screen">
+      {/* <!-- Left Panel --> */}
+      <div class="leftPanel max-w-[230px] w-full bg-white shadow-md relative z-30">
+        <div class="logo text-center px-4 pt-5 pb-8">
+          <a href="#" class="block">
+            <img
+              src={ logoImage } 
+              alt="Evento Package Logo"
+              class="max-w-full w-auto mx-auto"
+            />
+          </a>
+        </div>
+        <div class="nav">
+          <a href="#" class="active" title="Dashboard">
+            <span>
+              <i class="w-6 block text-center text-lg icon-deshbord"></i>
+            </span>
+            <span>Dashboard</span>
+          </a>
+          <a href="#" class="" title="Subscription">
+            <span>
+              <i class="w-6 block text-center text-lg icon-subsciption"></i>
+            </span>
+            <span>Subscription</span>
+          </a>
+          <a href="#" class="" title="Refer & Earn">
+            <span>
+              <i class="w-6 block text-center text-lg icon-refer"></i>
+            </span>
+            <span>Refer & Earn</span>
+          </a>
+          <a href="#" class="" title="Redeem">
+            <span>
+              <i class="w-6 block text-center text-lg icon-redem"></i>
+            </span>
+            <span>Redeem</span>
+          </a>
+          <a href="#" class="" title="Help & FAQ">
+            <span>
+              <i class="w-6 block text-center text-lg icon-help"></i>
+            </span>
+            <span>Help & FAQ</span>
+          </a>
+          <a href="#" class="" title="Gallery">
+            <span>
+              <i class="w-6 block text-center text-lg icon-gallery"></i>
+            </span>
+            <span>Gallery</span>
+          </a>
+          <a href="#" class="" title="Festum Evento">
+            <span>
+              <i class="w-6 block text-center text-lg icon-f-evanto"></i>
+            </span>
+            <span>Festum Evento</span>
+          </a>
+          <a href="#" class="" title="Booking">
+            <span>
+              <i class="w-6 block text-center text-lg icon-booking"></i>
+            </span>
+            <span>Booking</span>
+          </a>
+          <a href="#" class="" title="Invoice">
+            <span>
+              <i class="w-6 block text-center text-lg icon-invoice"></i>
+            </span>
+            <span>Invoice</span>
+          </a>
+          <a href="#" class="" title="Membership">
+            <span>
+              <i class="w-6 block text-center text-lg icon-membership"></i>
+            </span>
+            <span>Membership</span>
+          </a>
+        </div>
+      </div>
+      {/* <!-- Content --> */}
+      <div class="w-full">
+        {/* <!-- Top Header --> */}
+        <div class="w-full bg-white py-3.5 px-6 xl:px-12 flex flex-wrap items-center shadow-sm">
+          {/* <!-- Search Box --> */}
+          <form class="w-72 relative bg-brightGray rounded-md flex items-center">
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Search"
+              class="w-full h-10 bg-transparent text-sm font-bold pl-3.5 focus:outline-none"
+            />
+            <button type="submit" class="p-3.5">
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.80624 7.7457C9.39675 6.93931 9.74996 5.9488 9.74996 4.87502C9.74996 2.18703 7.56294 0 4.87497 0C2.18699 0 0 2.18703 0 4.87502C0 7.56301 2.18701 9.75004 4.87499 9.75004C5.94877 9.75004 6.93936 9.39678 7.74575 8.80627L10.9395 12L12 10.9395C12 10.9394 8.80624 7.7457 8.80624 7.7457ZM4.87499 8.25002C3.01391 8.25002 1.50001 6.73611 1.50001 4.87502C1.50001 3.01393 3.01391 1.50001 4.87499 1.50001C6.73607 1.50001 8.24997 3.01393 8.24997 4.87502C8.24997 6.73611 6.73605 8.25002 4.87499 8.25002Z"
+                  fill="#9BA0A8"
+                />
+              </svg>
+            </button>
+          </form>
+
+          {/* <!-- Right Bar --> */}
+          <div class="ml-auto">
+            <div class="flex items-center space-x-6">
+              <a
+                href="#"
+                class="block hover:text-spiroDiscoBall anim"
+                title="Language"
+              >
+                <span class="icon-language text-2xl block"></span>
+              </a>
+              <a
+                href="#"
+                class="block hover:text-spiroDiscoBall anim"
+                title="Massage"
+              >
+                <span class="icon-massage text-2xl block"></span>
+              </a>
+              <a
+                href="#"
+                class="block hover:text-spiroDiscoBall anim"
+                title="Megaphone"
+              >
+                <span class="icon-megaphone text-2xl block"></span>
+              </a>
+              <a href="#" class="block">
+                <img
+                  src={ userImage }
+                  alt="user name"
+                  class="w-12 h-12 object-cover rounded-2xl"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- Content In --> */}
+        <div class="rightInContent">
+          <div class="wrapper"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
