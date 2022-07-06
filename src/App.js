95d4e46b53bce3a750a472a3b7bb9c611943829f
brijeshsoni1300 <brijeshpatadiya19os@gmail.com>
2022-07-06 19:49:56 +0530

    Router setup done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import "./assest/css/style.css";
 import "./assest/css/fonts.css";
 import "./assest/css/moon-icon.css";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
 import SideBar from './component/SideBar';
 
 function App() {
   return (
     <div className="app">
-        <SideBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SideBar />} />
+            <Route path="/dashboard" element={<SideBar />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
