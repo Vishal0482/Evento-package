e4c7abaac82c04d1572b6920f3426f612649d949
Ajay Dabhi <ajay.scalelot@gmail.com>
2022-10-05 10:47:47 +0530

    Ajay - pull on 05-10

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,10 +1,55 @@
+import axios from 'axios';
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 
-function Login() {
-  return (
-    <div className="main">
+
+const Login = () => {
+	const navigator = useNavigate();
+	const [login, setLogin] = useState({ email: "", password: "" });
+	const [passVisible, setPassVisible] = useState(false);
+	const [error, setError] = useState(false);
+
+
+	const handelChange = (name, value) => {
+		setLogin({ ...login, [name]: value })
+	}
+	const handelLogin = async (e) => {
+		e.preventDefault();
+		console.log(login);
+		try {
+			if (login.email !== "") {
+				if (login.password !== "") {
+					const response = await axios.post("http://64.227.167.195:8000/api/login", { username: login.email, password: login.password });
+					console.log(response.data);
+					if (response.data.isSuccess === true) {
+						alert("login Success")
+						navigator("/dashboard/event")
+					} else {
+						setError("Email And Password Not Valid")
+					}
+				}
+				else {
+
+					setError("password not Empty")
+				}
+			}
+			else {
+				setError("Email Address Required")
+
+			}
+
+
+
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	// console.log(login);
+
+	return (
+		<div className="main">
 			<div className="login-page">
 				<div className="form-holder">
 					<div className="form-main">
@@ -12,48 +57,35 @@ function Login() {
 							<h1>Welcome Back!</h1>
 							<p>Log in with your data that you entered during your registration</p>
 						</div>
+						{error && <span style={{ color: "red" }}>{error}</span>}
 						<div className="form">
-							{/* {errMsg && <span style={{ color: "red" }}>Username or Password incorrect</span>} */}
-							<form action="" method="post" >
+							<form action="" method="post" onSubmit={handelLogin}>
 								<div className="ps-1">
 									<label htmlFor="">Email or Phone number</label>
-									{/* <input type="text" id="" name="" value={userData.emailOrPhone} onChange={(e) => { setFormField('emailOrPhone', e.target.value) }} /> */}
-									<input type="text" id="" name="" />
+									<input type="email" name="email" value={login.email} onChange={(e) => { handelChange("email", e.target.value) }} />
+									{/* {error && <span style={{ color: "red" }}>{error}</span>} */}
 								</div>
 								<div className="ps-1">
 									<label htmlFor="">Password</label>
-									{/* <input type="password" id="" name="" value={userData.password} onChange={(e) => { setFormField('password', e.target.value) }} /> */}
-									<input type="password" id="" name="" />
+									<input type={(passVisible) ? "text" : "password"} name="password" value={login.password} onChange={(e) => { handelChange("password", e.target.value) }} />
+									{/* {error && <span style={{ color: "red" }}>{error}</span>} */}
+									<i onClick={() => setPassVisible(!passVisible)} className="icon-view"></i>
 								</div>
 
 								<Link to="/forgatepassword"> Forget Password?</Link>
-								{/* <button type="button" onClick={handleSubmit}>LOGIN NOW</button> */}
-								<button type="button" >LOGIN NOW</button>
+								<button type="Submit">LOGIN NOW</button>
 							</form>
 						</div>
-						{/* <div className="btn-hr">
-							<p>or</p>
-						</div>
-						<div className="social-holder">
-							<a href="https://www.google.com/" className="social-btn btn1">
-								<img src={googlelogo} alt="goooglelogo" />
-								<p>Google</p>
-							</a>
-							<a href="https://www.google.com/" className="social-btn btn2">
-								<img src={facebooklogo} alt="facebooklogo" />
-								<p>Facebook</p>
-							</a>
-						</div> */}
 						<div className="botm-t">
 							<p>
-								Are You new? <Link to="/register">Registrater Now</Link>
+								Are You new? <Link to="/dashboard/register">Registrater Now</Link>
 							</p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-  )
+	)
 }
 
 export default Login
