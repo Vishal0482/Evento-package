0b7d43fe824646429eb13fcebfa247a3adb3b645
AjayScalelot <ajay.scalelot@gmail.com>
2022-10-13 18:13:52 +0530

    update 13-10

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,12 +1,10 @@
-import axios from 'axios';
-import React from 'react'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom'
-import { addLogin } from '../redux/createLogin'
-import {baseUrl} from '.././config'
-
-
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { addLogin } from "../redux/createLogin";
+import { baseUrl } from ".././config";
 
 const Login = () => {
 	const navigator = useNavigate();
@@ -14,12 +12,11 @@ const Login = () => {
 	const [passVisible, setPassVisible] = useState(false);
 	const [error, setError] = useState(false);
 	const dispatch = useDispatch();
-	const token = useSelector(state => state.createLogin?.token);
-
+	const token = useSelector((state) => state.createLogin?.token);
 
 	const handelChange = (name, value) => {
-		setLogin({ ...login, [name]: value })
-	}
+		setLogin({ ...login, [name]: value });
+	};
 	const handelLogin = async (e) => {
 		e.preventDefault();
 		console.log(login);
@@ -29,27 +26,22 @@ const Login = () => {
 					const response = await axios.post(`${baseUrl}/api/login`, { username: login.email, password: login.password });
 					console.log(response.data);
 					if (response.data.isSuccess === true) {
-						alert("login Success")
-						dispatch(addLogin({ email: login.email, token: response.data.data.token,userId:response.data.data.userId }))
-						navigator("/dashboard/event/id")
+						alert("login Success");
+						dispatch(addLogin({ email: login.email, token: response.data.data.token, userId: response.data.data.userId }));
+						navigator("/dashboard/event/id");
 					} else {
-						setError("Email And Password Not Valid")
+						setError("Email And Password Not Valid");
 					}
+				} else {
+					setError("password not Empty");
 				}
-				else {
-
-					setError("password not Empty")
-				}
+			} else {
+				setError("Email Address Required");
 			}
-			else {
-				setError("Email Address Required")
-
-			}
-
 		} catch (error) {
 			console.log(error);
 		}
-	}
+	};
 	if (!token)
 		return (
 			<div className="main">
@@ -65,12 +57,26 @@ const Login = () => {
 								<form action="" method="post" onSubmit={handelLogin}>
 									<div className="ps-1">
 										<label htmlFor="">Email or Phone number</label>
-										<input type="email" name="email" value={login.email} onChange={(e) => { handelChange("email", e.target.value) }} />
+										<input
+											type="email"
+											name="email"
+											value={login.email}
+											onChange={(e) => {
+												handelChange("email", e.target.value);
+											}}
+										/>
 										{/* {error && <span style={{ color: "red" }}>{error}</span>} */}
 									</div>
 									<div className="ps-1">
 										<label htmlFor="">Password</label>
-										<input type={(passVisible) ? "text" : "password"} name="password" value={login.password} onChange={(e) => { handelChange("password", e.target.value) }} />
+										<input
+											type={passVisible ? "text" : "password"}
+											name="password"
+											value={login.password}
+											onChange={(e) => {
+												handelChange("password", e.target.value);
+											}}
+										/>
 										{/* {error && <span style={{ color: "red" }}>{error}</span>} */}
 										<i onClick={() => setPassVisible(!passVisible)} className="icon-view"></i>
 									</div>
@@ -88,11 +94,10 @@ const Login = () => {
 					</div>
 				</div>
 			</div>
-		)
-
+		);
 	else {
-		navigator("/dashboard/event/id")
+		navigator("/dashboard/event/id");
 	}
-}
+};
 
-export default Login
+export default Login;
