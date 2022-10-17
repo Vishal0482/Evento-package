4d75c26d1e5d502ff76ae4eddc9f5266410000f9
AjayScalelot <ajay.scalelot@gmail.com>
2022-10-17 11:26:35 +0530

    Changing to warnning Slow

--- src/component/events/EventAboutPlace.js
+++ src/component/events/EventAboutPlace.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react'
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Advertisement from "../Advertisement";
-import { useDispatch } from 'react-redux';
-import { addAboutPlace } from '../../redux/createEvent';
+import { useDispatch } from "react-redux";
+import { addAboutPlace } from "../../redux/createEvent";
 
 function EventAboutPlace() {
-
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 	const [price, setPrice] = useState("");
@@ -15,138 +14,169 @@ function EventAboutPlace() {
 	console.log(price, priceType, about);
 
 	const clickNextHandler = () => {
-		const aboutPlace = {price: price, priceType: priceType, about: about};
-		dispatch(addAboutPlace({aboutPlace: aboutPlace}));
+		const aboutPlace = { price: price, priceType: priceType, about: about };
+		dispatch(addAboutPlace({ aboutPlace: aboutPlace }));
 		navigate("/dashboard/event/personaldetails");
-	}
-  
-  return (
-	//  <!-- Content In -->
-	 <div className="rightInContent">
-	 <div className="wrapper min-h-full flex flex-col">
-	   <div className="space-y-8 h-full">
-		 {/* <!-- title-holder  -->/ */}
-		 <div className="flex justify-between items-center">
-		   <Link to="/" className="flex items-center"><i className="icon-back-arrow mr-4 text-2xl"></i>
-			 <h1>Sweet Love Catering</h1>
-		   </Link>
-		 </div>
-		 {/* <!-- step-progress-bar  --> */}
-		 <div className="w-full overflow-hidden">
-		   <ul className="flex justify-between step-progress-holder">
-			 <li>
-			   <div>
-				 <span>01</span>
-			   </div>
-			   <h3>Add Place</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>02</span>
-			   </div>
-			   <h3>about place</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>03</span>
-			   </div>
-			   <h3>personal details</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>04</span>
-			   </div>
-			   <h3>Photos & videos</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>05</span>
-			   </div>
-			   <h3>add service</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>06</span>
-			   </div>
-			   <h3>capacity</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>07</span>
-			   </div>
-			   <h3>company details</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>08</span>
-			   </div>
-			   <h3>Terms & Conditions</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>09</span>
-			   </div>
-			   <h3>Discount</h3>
-			 </li>
-			 <li>
-			   <div>
-				 <span>10</span>
-			   </div>
-			   <h3>Calendar</h3>
-			 </li>
-		   </ul>
-		 </div>
-		 {/* <!-- main-content  --> */}
-		 <div className="space-y-3">
-		   <div className="w-full">
-			 <span className="input-titel">Price</span>
-			 <label htmlFor="" className="flex items-center w-full bg-white p-2 px-3.5 rounded-md">
-			   <div className="w-full px-3.5">
-				 <input type="text" className="w-full outline-none text-spiroDiscoBall font-bold text-base"
-				   value={price} onChange={(e) => setPrice(e.target.value)} />
-			   </div>
-			   <div className="selectPrice flex items-center space-x-3">
-				 <label className="block cursor-pointer">
-				   <input type="radio" name="price" value="perDay" className="hidden" onChange={(e) => setPriceType("per_day")} />
-				   <span
-					 className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
-					 Per / Day
-				   </span>
-				 </label>
-				 <label className="block cursor-pointer">
-				   <input type="radio" name="price" value="perHour" className="hidden" defaultChecked onChange={(e) => setPriceType("per_hour")} />
-				   <span
-					 className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
-					 Per / Hour
-				   </span>
-				 </label>
-				 <label className="block cursor-pointer">
-				   <input type="radio" name="price" value="perEvent" className="hidden"onChange={(e) => setPriceType("per_event")} />
-				   <span
-					 className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
-					 Per / Event
-				   </span>
-				 </label>
-			   </div>
-			 </label>
-		   </div>
-		   <div className="w-full">
-			 <span className="input-titel">About place</span>
-			 <textarea name="" id="" cols="30" rows="5"
-			   className="outline-none flex items-center w-full bg-white p-2 px-3.5 rounded-md" onChange={(e) => setAbout(e.target.value)}></textarea>
-		   </div>
-		 </div>
-		 {/* <!-- advisement --> */}
-		<Advertisement />
-	   </div>
-	   <div className="prw-next-btn mt-auto">
-		 <button type="button" className="flex items-center" onClick={() => navigate(-1)}><i className="icon-back-arrow mr-3"></i><h3>Back</h3></button>
-		 <button type="button" className="flex items-center active" onClick={clickNextHandler}><h3>Next</h3><i className="icon-next-arrow ml-3"></i></button>
-	   </div>
-	 </div>
+	};
 
-   </div>
-  )
+	return (
+		//  <!-- Content In -->
+		<div className="rightInContent">
+			<div className="wrapper min-h-full flex flex-col">
+				<div className="space-y-8 h-full">
+					{/* <!-- title-holder  -->/ */}
+					<div className="flex justify-between items-center">
+						<Link to="/" className="flex items-center">
+							<i className="icon-back-arrow mr-4 text-2xl"></i>
+							<h1>Sweet Love Catering</h1>
+						</Link>
+					</div>
+					{/* <!-- step-progress-bar  --> */}
+					<div className="w-full overflow-hidden">
+						<ul className="flex justify-between step-progress-holder">
+							<li>
+								<div>
+									<span>01</span>
+								</div>
+								<h3>Add Place</h3>
+							</li>
+							<li>
+								<div>
+									<span>02</span>
+								</div>
+								<h3>about place</h3>
+							</li>
+							<li>
+								<div>
+									<span>03</span>
+								</div>
+								<h3>personal details</h3>
+							</li>
+							<li>
+								<div>
+									<span>04</span>
+								</div>
+								<h3>Photos & videos</h3>
+							</li>
+							<li>
+								<div>
+									<span>05</span>
+								</div>
+								<h3>add service</h3>
+							</li>
+							<li>
+								<div>
+									<span>06</span>
+								</div>
+								<h3>capacity</h3>
+							</li>
+							<li>
+								<div>
+									<span>07</span>
+								</div>
+								<h3>company details</h3>
+							</li>
+							<li>
+								<div>
+									<span>08</span>
+								</div>
+								<h3>Terms & Conditions</h3>
+							</li>
+							<li>
+								<div>
+									<span>09</span>
+								</div>
+								<h3>Discount</h3>
+							</li>
+							<li>
+								<div>
+									<span>10</span>
+								</div>
+								<h3>Calendar</h3>
+							</li>
+						</ul>
+					</div>
+					{/* <!-- main-content  --> */}
+					<div className="space-y-3">
+						<div className="w-full">
+							<span className="input-titel">Price</span>
+							<label htmlFor="" className="flex items-center w-full bg-white p-2 px-3.5 rounded-md">
+								<div className="w-full px-3.5">
+									<input
+										type="text"
+										className="w-full outline-none text-spiroDiscoBall font-bold text-base"
+										value={price}
+										onChange={(e) => setPrice(e.target.value)}
+									/>
+								</div>
+								<div className="selectPrice flex items-center space-x-3">
+									<label className="block cursor-pointer">
+										<input
+											type="radio"
+											name="price"
+											value="perDay"
+											className="hidden"
+											onChange={(e) => setPriceType("per_day")}
+										/>
+										<span className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
+											Per / Day
+										</span>
+									</label>
+									<label className="block cursor-pointer">
+										<input
+											type="radio"
+											name="price"
+											value="perHour"
+											className="hidden"
+											defaultChecked
+											onChange={(e) => setPriceType("per_hour")}
+										/>
+										<span className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
+											Per / Hour
+										</span>
+									</label>
+									<label className="block cursor-pointer">
+										<input
+											type="radio"
+											name="price"
+											value="perEvent"
+											className="hidden"
+											onChange={(e) => setPriceType("per_event")}
+										/>
+										<span className="text-sm text-quicksilver py-2 px-3 bg-white shadow-lg whitespace-nowrap font-bold rounded block">
+											Per / Event
+										</span>
+									</label>
+								</div>
+							</label>
+						</div>
+						<div className="w-full">
+							<span className="input-titel">About place</span>
+							<textarea
+								name=""
+								id=""
+								cols="30"
+								rows="5"
+								className="outline-none flex items-center w-full bg-white p-2 px-3.5 rounded-md"
+								onChange={(e) => setAbout(e.target.value)}></textarea>
+						</div>
+					</div>
+					{/* <!-- advisement --> */}
+					<Advertisement />
+				</div>
+				<div className="prw-next-btn mt-auto">
+					<button type="button" className="flex items-center" onClick={() => navigate(-1)}>
+						<i className="icon-back-arrow mr-3"></i>
+						<h3>Back</h3>
+					</button>
+					<button type="button" className="flex items-center active" onClick={clickNextHandler}>
+						<h3>Next</h3>
+						<i className="icon-next-arrow ml-3"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default EventAboutPlace
+export default EventAboutPlace;
--- src/component/events/EventAddPlaces.js
+++ src/component/events/EventAddPlaces.js
@@ -1,50 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Modal from "../modal/Modal";
-import EventAddPlacesEventList from './EventAddPlacesEventList';
+import EventAddPlacesEventList from "./EventAddPlacesEventList";
 import EventPopUpAddPlaceWithDisplayName from "./popups/EventPopUpAddPlaceWithDisplayName";
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import Advertisement from '../Advertisement';
-import StepProgressBar from './StepProgressBar';
-
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import Advertisement from "../Advertisement";
+import StepProgressBar from "./StepProgressBar";
 
 function EventAddPlaces() {
-
 	const [isAddPlaceWithDisplayNamePopUpOpen, setIsAddPlaceWithDisplayNamePopUpOpen] = useState(false);
 	const navigate = useNavigate();
 	const newEvent = useSelector((state) => state.createEvent.category);
 	console.log(newEvent);
-  return (
-	//  <!-- Content In -->
-	 <div className="rightInContent">
-	 <div className="wrapper min-h-full flex flex-col">
-	   <div className="space-y-8 h-full">
-		 {/* <!-- title-holder  --> */}
-		 <div className="flex justify-between items-center">
-			 <a href="#" className="flex items-center"><i className="icon-back-arrow mr-4 text-2xl"></i><h1>Create New</h1></a>
-			 <button href="#" onClick={()=>setIsAddPlaceWithDisplayNamePopUpOpen(true)} className="btn-primary small"><i className="icon-plus mr-3"></i>Add place</button>
-		 </div>
-		 {/* <!-- step-progress-bar  --> */}
-		<StepProgressBar />
-		 {/* <!-- main-content  --> */}
-		 <div className=" space-y-3">
-		   <EventAddPlacesEventList displayName={newEvent?.displayName} categoryName={newEvent?.categoryName} />
-		 </div>
-		 {/* <!-- advisement --> */}
-		 <Advertisement />
-	   </div>
-	   <div className="prw-next-btn mt-auto">
-		 <button type="button" className="flex items-center" onClick={() => navigate(-1)}><i className="icon-back-arrow mr-3"></i><h3>Back</h3></button>
-		 <button type="button" className="flex items-center active" onClick={() => navigate("/dashboard/event/aboutplace")}><h3>Next</h3><i className="icon-next-arrow ml-3"></i></button>
-	   </div>
-	 </div>
-	 <Modal isOpen={isAddPlaceWithDisplayNamePopUpOpen}>
-		<EventPopUpAddPlaceWithDisplayName handleClose={setIsAddPlaceWithDisplayNamePopUpOpen}/>
-	 </Modal>
-	 
-   </div>
-
-  )
+	return (
+		//  <!-- Content In -->
+		<div className="rightInContent">
+			<div className="wrapper min-h-full flex flex-col">
+				<div className="space-y-8 h-full">
+					{/* <!-- title-holder  --> */}
+					<div className="flex justify-between items-center">
+						<Link to="/" className="flex items-center">
+							<i className="icon-back-arrow mr-4 text-2xl"></i>
+							<h1>Create New</h1>
+						</Link>
+						<button href="#" onClick={() => setIsAddPlaceWithDisplayNamePopUpOpen(true)} className="btn-primary small">
+							<i className="icon-plus mr-3"></i>Add place
+						</button>
+					</div>
+					{/* <!-- step-progress-bar  --> */}
+					<StepProgressBar />
+					{/* <!-- main-content  --> */}
+					<div className=" space-y-3">
+						<EventAddPlacesEventList displayName={newEvent?.displayName} categoryName={newEvent?.categoryName} />
+					</div>
+					{/* <!-- advisement --> */}
+					<Advertisement />
+				</div>
+				<div className="prw-next-btn mt-auto">
+					<button type="button" className="flex items-center" onClick={() => navigate(-1)}>
+						<i className="icon-back-arrow mr-3"></i>
+						<h3>Back</h3>
+					</button>
+					<button type="button" className="flex items-center active" onClick={() => navigate("/dashboard/event/aboutplace")}>
+						<h3>Next</h3>
+						<i className="icon-next-arrow ml-3"></i>
+					</button>
+				</div>
+			</div>
+			<Modal isOpen={isAddPlaceWithDisplayNamePopUpOpen}>
+				<EventPopUpAddPlaceWithDisplayName handleClose={setIsAddPlaceWithDisplayNamePopUpOpen} />
+			</Modal>
+		</div>
+	);
 }
 
-export default EventAddPlaces
+export default EventAddPlaces;
