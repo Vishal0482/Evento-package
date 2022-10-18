fed23249658a11984a75055fceca261a8595f2e5
AjayScalelot <ajay.sclelot@gmail.com>
2022-10-18 10:54:56 +0530

    Change in token

---
+++ src/redux/stepCountPogress.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  
+}
+
+export const stepCountPogress = createSlice({
+  name: 'stepCountPogress',
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.
+    }
+  }
+});
