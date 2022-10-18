2ae7241b0d103caf2a38157f16f0b1f34a7f3e3c
AjayScalelot <ajay.sclelot@gmail.com>
2022-10-18 16:44:18 +0530

    event Catagory name

--- src/component/events/DashboardEventCategoryItem.js
+++ src/component/events/DashboardEventCategoryItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import sweetLoveCatering from "../../assest/images/sweet-love-catering.png";
 
-function DashboardEventCategoryItem(data) {
+function DashboardEventCategoryItem({data}) {
 	return (
 		<div className="w-full flex items-center">
 			<div>
@@ -18,7 +18,7 @@ function DashboardEventCategoryItem(data) {
 						<div className="flex justify-between border-b-2 pb-4">
 							<div className="">
 								<span className="text-sm text-white bg-spiroDiscoBall px-3 py-1">{data?.event_type}</span>
-								<h2 className="text-japaneseIndigo pt-5">{data.display_name}</h2>
+								<h2 className="text-japaneseIndigo pt-5">{data?.display_name}</h2>
 								<div className="text-sm text-quicksilver pt-3">
 									<i className="icon-fill-location mr-3"></i>Dallas, Texas
 								</div>
