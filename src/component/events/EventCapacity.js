db234b28ea88bc9d4067aab4042ff3f6360df195
Vishal0482 <vishal482parmar@gmail.com>
2022-11-28 13:38:14 +0530

    get capacity done. (post pending).

--- src/component/events/EventCapacity.js
+++ src/component/events/EventCapacity.js
@@ -7,6 +7,8 @@ import { decrement, increment } from '../../redux/stepProgressCount';
 import { toast, ToastContainer } from 'react-toastify';
 import { baseUrl } from '../../config';
 import axios from 'axios';
+import { onlyDigits } from "../../shared/constants";
+import { useEffect } from 'react';
 
 function EventCapacity() {
   const displayName = localStorage.getItem("displayName");
@@ -20,15 +22,18 @@ function EventCapacity() {
 		'Authorization': `Token ${token}`
 	}
 
-  const [type, setType] = useState("");
+  const [type, setType] = useState("romantic_stay");
 	const initialState = {
-		event: eventId,
+		eventid: eventId,
     person_capacity: "",
     parking_capacity: "",
     address: "",
-    let: "",
-    long: ""
+    location: {
+      type: "Point",
+      coordinates: []
+    }
 	}
+  
 	const [values, setValues] = useState(initialState);
 	const handleInputChange = (e) => {
 		const { name, value } = e.target;
@@ -39,14 +44,43 @@ function EventCapacity() {
 	  };
 	console.log(values);
 
+  const getCapacity = async() => {
+		try {
+			const response = await axios.get(`${baseUrl}/organizer/events/capacity?eventid=${eventId}`, {headers: header});
+			if(response.data.Data.capacity){
+        setValues(response.data.Data.capacity);
+        if('facilities' in response.data.Data.capacity) {
+          setType(response.data.Data.capacity?.facilities);
+        }
+			}
+			if(!response.data.IsSuccess) {
+				toast.error("Error occured while fetching data.")
+			}
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	useEffect(()=>{
+		getCapacity();
+	},[]);
+
 	const clickNextHandler = async() => {
+    if(!onlyDigits.test(values.parking_capacity.trim()) || !onlyDigits.test(values.person_capacity.trim())) {
+      toast.warn("Please Enter valid capacity.");
+      return
+    }
     try {
-      const response = await axios.post(`${baseUrl}/api/event/capacity`, {...values, IncludingFacilities: type},{headers: header});
-      toast.success("Data Saved Successfully.");  
-      dispatch(increment());
-      navigate(`../companydetails/${params.eventId}/${params.userId}`);
+      const response = await axios.post(`${baseUrl}/organizer/events/capacity`, {...values, facilities: type},{headers: header});
+      if(response.data.IsSuccess) {
+        toast.success(response.data.Message);  
+        dispatch(increment());
+        navigate(`../companydetails`);
+      } else {
+        toast.error(response.data.Message);  
+      }
     } catch (error) {
-      toast.error("Something went Wrong.")
+      toast.error("Something went Wrong.");
       console.log(error);
     }
 
@@ -59,7 +93,7 @@ function EventCapacity() {
 
   return (
 		//   <!-- Content In -->
-		  <div className="rightInContent">
+		  <div>
           <div className="wrapper min-h-full">
             
             <div className="space-y-8 h-full">
@@ -74,19 +108,19 @@ function EventCapacity() {
                 <div className="flex items-end -mx-3.5">
                   <div className="w-full lg:w-1/3 px-3.5">
                       <label htmlfor="selact" className="p-5 py-4 bg-white rounded-md flex space-x-3 cursor-pointer">
-                        <input type="radio" name="type" id="select" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" onChange={(e) => setType("romantic_stay")}/>
+                        <input type="radio" name="type" id="select" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" checked={type === "romantic_stay" && true} onChange={(e) => setType("romantic_stay")}/>
                         <h3 className="text-base">Romantic Stay</h3>
                       </label>
                   </div>
                   <div className="w-full lg:w-1/3 px-3.5">
                       <label htmlfor="selact1" className="p-5 py-4 bg-white rounded-md flex space-x-3 cursor-pointer">
-                        <input type="radio" name="type" id="select1" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" onChange={(e) => setType('romantic_lunch_dinner')} />
+                        <input type="radio" name="type" id="select1" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" checked={type === "romantic_lunch_dinner" && true} onChange={(e) => setType('romantic_lunch_dinner')} />
                         <h3 className="text-base">Romantic Lunch / Dinner</h3>
                       </label>
                   </div>
                   <div className="w-full lg:w-1/3 px-3.5">
                     <label htmlfor="selact2" className="p-5 py-4 bg-white rounded-md flex space-x-3 cursor-pointer">
-                      <input type="radio" name="type" id="select2" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" onChange={(e) => setType('romantic_candlelight_dinner')} />
+                      <input type="radio" name="type" id="select2" className="w-6 h-6 rounded-full bg-brightGray appearance-none cursor-pointer" checked={type === "romantic_candlelight_dinner" && true} onChange={(e) => setType('romantic_candlelight_dinner')} />
                       <h3 className="text-base">Romantic Candlelight Dinner</h3>
                     </label>
                   </div>
@@ -102,8 +136,9 @@ function EventCapacity() {
                 <div className="w-full relative">
                     <span className="input-titel">Address</span>
                     <div className="w-full flex flex-wrap bg-white p-2 rounded-md">
-                      <textarea id="" cols="30" rows="5" className="outline-none w-full" name="address" value={values.address} onChange={handleInputChange} ></textarea>
-                      <a href={`https://www.google.com/maps/place/${values.address?.replaceAll(" ","+")}`} target="blank" className="title-btn ml-auto uppercase">Location from Google map</a>
+                    <iframe className="min-h-[170px] xl:min-h-[220px]" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14875.88775631223!2d72.81608609999999!3d21.2329613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1655442825672!5m2!1sen!2sin" width="100%" height="100%" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='google-map'></iframe>
+                      {/* <textarea id="" cols="30" rows="5" className="outline-none w-full" name="address" value={values.address} onChange={handleInputChange} ></textarea> */}
+                      {/* <a href={`https://www.google.com/maps/place/${values.address?.replaceAll(" ","+")}`} target="blank" className="title-btn ml-auto uppercase">Location from Google map</a> */}
                     </div>                    
                 </div>
               </div>
